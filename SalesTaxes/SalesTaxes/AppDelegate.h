45f79f3f5e5293971e312ed9a6b31d5882002ff9
William Izzo <iwillyiam@hotmail.com>
2016-11-15 21:58:42 +0100

    Initial commit

---
+++ SalesTaxes/SalesTaxes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SalesTaxes
+//
+//  Created by William Izzo on 15/11/2016.
+//  Copyright © 2016 wizzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SalesTaxes/SalesTaxes/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  SalesTaxes
+//
+//  Created by William Izzo on 15/11/2016.
+//  Copyright © 2016 wizzo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
